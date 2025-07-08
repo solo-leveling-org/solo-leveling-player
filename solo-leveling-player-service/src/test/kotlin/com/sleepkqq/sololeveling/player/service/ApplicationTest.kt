cded006bef910324f00d16b35d8d5af5055d5c7f
sleepkqq <nikita32ivanov@gmail.com>
2025-07-08 22:52:42 +0300

    Added PostgreSQL container for integration tests.

--- solo-leveling-player-service/src/test/kotlin/com/sleepkqq/sololeveling/player/service/ApplicationTest.kt
+++ solo-leveling-player-service/src/test/kotlin/com/sleepkqq/sololeveling/player/service/ApplicationTest.kt
@@ -1,11 +1,23 @@
 package com.sleepkqq.sololeveling.player.service
 
+import com.sleepkqq.sololeveling.player.model.entity.task.Task
+import com.sleepkqq.sololeveling.player.service.service.task.TaskService
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
 
 class ApplicationTest : BaseTestClass() {
 
+	@Autowired
+	lateinit var taskService: TaskService
+
 	@Test
 	fun contextLoads() {
+		val taskId = UUID.randomUUID()
+		taskService.insert(Task { id = taskId })
 
+		val inserted = taskService.find(taskId)
+		assertThat(inserted).isNotNull()
 	}
 }
