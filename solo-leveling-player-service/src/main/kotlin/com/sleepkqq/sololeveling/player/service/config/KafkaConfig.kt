0883923c0c9b4c1b682d194452f1252095f9582d
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-04 09:43:22 +0300

    Updated proto dependency and mapper.

--- solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/config/KafkaConfig.kt
+++ solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/config/KafkaConfig.kt
@@ -14,6 +14,7 @@ import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.core.ProducerFactory
 
+@Suppress("unused")
 @EnableKafka
 @Configuration
 class KafkaConfig(
--- solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/mapper/ProtoMapper.kt
+++ solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/mapper/ProtoMapper.kt
@@ -22,55 +22,55 @@ import java.util.UUID
 @Mapper(
 	componentModel = "spring",
 	unmappedTargetPolicy = ReportingPolicy.IGNORE,
-	collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED
+	collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED,
+	nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT,
+	nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_DEFAULT
 )
 abstract class ProtoMapper {
 
 	@Named("toEntityTaskTopic")
-	fun map(taskTopic: com.sleepkqq.sololeveling.proto.player.TaskTopic): TaskTopic =
-		TaskTopic.valueOf(taskTopic.name)
+	fun map(input: com.sleepkqq.sololeveling.proto.player.TaskTopic): TaskTopic =
+		TaskTopic.valueOf(input.name)
 
 	@Named("toEntityAssessment")
-	fun map(assessment: com.sleepkqq.sololeveling.proto.player.Assessment): Assessment =
-		Assessment.valueOf(assessment.name)
+	fun map(input: com.sleepkqq.sololeveling.proto.player.Assessment): Assessment =
+		Assessment.valueOf(input.name)
 
 	@Named("toEntityPlayerTaskStatus")
-	fun map(playerTaskStatus: com.sleepkqq.sololeveling.proto.player.PlayerTaskStatus): PlayerTaskStatus =
-		PlayerTaskStatus.valueOf(
-			playerTaskStatus.name
-		)
+	fun map(input: com.sleepkqq.sololeveling.proto.player.PlayerTaskStatus): PlayerTaskStatus =
+		PlayerTaskStatus.valueOf(input.name)
 
 	@Named("toEntityTaskRarity")
-	fun map(taskRarity: com.sleepkqq.sololeveling.proto.player.TaskRarity): TaskRarity =
-		TaskRarity.valueOf(taskRarity.name)
+	fun map(input: com.sleepkqq.sololeveling.proto.player.TaskRarity): TaskRarity =
+		TaskRarity.valueOf(input.name)
 
 	@Named("toEntityUserRole")
-	fun map(userRole: com.sleepkqq.sololeveling.proto.user.UserRole): UserRole =
-		UserRole.valueOf(userRole.name)
+	fun map(input: com.sleepkqq.sololeveling.proto.user.UserRole): UserRole =
+		UserRole.valueOf(input.name)
 
 	@Named("toSoulCoins")
 	fun map(input: BigDecimal): Money = input.toMoney("SLCN")
 
 	@Named("toProtoPlayerBalance")
 	@Mapping(target = "balance", source = "balance", qualifiedByName = ["toSoulCoins"])
-	abstract fun map(playerBalance: UserView.TargetOf_player.TargetOf_balance): com.sleepkqq.sololeveling.proto.player.PlayerBalanceView
+	abstract fun map(input: UserView.TargetOf_player.TargetOf_balance): com.sleepkqq.sololeveling.proto.player.PlayerBalanceView
 
 	@Named("toProtoPlayer")
 	@Mapping(target = "balance", source = "balance", qualifiedByName = ["toProtoPlayerBalance"])
 	abstract fun map(input: UserView.TargetOf_player): com.sleepkqq.sololeveling.proto.player.PlayerView
 
-	abstract fun map(playerTaskTopicView: PlayerTaskTopicView): com.sleepkqq.sololeveling.proto.player.PlayerTaskTopicView
+	abstract fun map(input: PlayerTaskTopicView): com.sleepkqq.sololeveling.proto.player.PlayerTaskTopicView
 
-	abstract fun map(playerTaskView: PlayerTaskView): com.sleepkqq.sololeveling.proto.player.PlayerTaskView
+	abstract fun map(input: PlayerTaskView): com.sleepkqq.sololeveling.proto.player.PlayerTaskView
 
 	@Mapping(target = "player", source = "player", qualifiedByName = ["toProtoPlayer"])
-	abstract fun map(userView: UserView): com.sleepkqq.sololeveling.proto.user.UserView
+	abstract fun map(input: UserView): com.sleepkqq.sololeveling.proto.user.UserView
 
-	fun map(playerTaskInput: com.sleepkqq.sololeveling.proto.player.PlayerTaskInput): PlayerTaskInput =
+	fun map(input: com.sleepkqq.sololeveling.proto.player.PlayerTaskInput): PlayerTaskInput =
 		PlayerTaskInput(
-			id = UUID.fromString(playerTaskInput.id),
-			version = playerTaskInput.version,
-			status = PlayerTaskStatus.valueOf(playerTaskInput.status.name)
+			id = UUID.fromString(input.id),
+			version = input.version,
+			status = PlayerTaskStatus.valueOf(input.status.name)
 		)
 
 	fun map(input: com.sleepkqq.sololeveling.proto.user.UserInput): UserInput =
