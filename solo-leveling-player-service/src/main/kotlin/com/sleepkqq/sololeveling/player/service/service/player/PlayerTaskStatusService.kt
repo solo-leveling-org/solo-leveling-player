e8ef26508fa480661999be358bbf28c9f3a3858f
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-05 08:26:56 +0300

    Refactored player task status updates.

--- solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/service/player/PlayerTaskStatusService.kt
+++ solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/service/player/PlayerTaskStatusService.kt
@@ -7,5 +7,6 @@ interface PlayerTaskStatusService {
 
 	fun skipTask(playerTask: PlayerTask, playerId: Long, now: LocalDateTime = LocalDateTime.now())
 	fun pendingCompleteTask(playerTask: PlayerTask, now: LocalDateTime = LocalDateTime.now())
+	fun inProgressTasks(tasks: Collection<PlayerTask>, now: LocalDateTime = LocalDateTime.now())
 	fun generateTasks(playerId: Long)
 }
--- solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/service/player/impl/PlayerTaskStatusServiceImpl.kt
+++ solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/service/player/impl/PlayerTaskStatusServiceImpl.kt
@@ -2,9 +2,10 @@ package com.sleepkqq.sololeveling.player.service.service.player.impl
 
 import com.sleepkqq.sololeveling.player.model.entity.player.PlayerTask
 import com.sleepkqq.sololeveling.player.model.entity.player.enums.PlayerTaskStatus
+import com.sleepkqq.sololeveling.player.model.repository.player.PlayerTaskRepository
 import com.sleepkqq.sololeveling.player.service.kafka.producer.GenerateTasksProducer
-import com.sleepkqq.sololeveling.player.service.service.player.PlayerTaskService
 import com.sleepkqq.sololeveling.player.service.service.player.PlayerTaskStatusService
+import org.babyfish.jimmer.sql.ast.mutation.SaveMode
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
@@ -12,24 +13,46 @@ import java.time.LocalDateTime
 @Suppress("unused")
 @Service
 class PlayerTaskStatusServiceImpl(
-	private val playerTaskService: PlayerTaskService,
+	private val playerTaskRepository: PlayerTaskRepository,
 	private val generateTasksProducer: GenerateTasksProducer
 ) : PlayerTaskStatusService {
 
 	@Transactional
 	override fun skipTask(playerTask: PlayerTask, playerId: Long, now: LocalDateTime) {
-		playerTaskService.setStatus(listOf(playerTask), PlayerTaskStatus.SKIPPED, now)
+		setStatus(listOf(playerTask), PlayerTaskStatus.SKIPPED, now)
 
 		generateTasks(playerId)
 	}
 
 	@Transactional
 	override fun pendingCompleteTask(playerTask: PlayerTask, now: LocalDateTime) {
-		playerTaskService.setStatus(listOf(playerTask), PlayerTaskStatus.PENDING_COMPLETION, now)
+		setStatus(listOf(playerTask), PlayerTaskStatus.PENDING_COMPLETION, now)
 	}
+
+	@Transactional
+	override fun inProgressTasks(tasks: Collection<PlayerTask>, now: LocalDateTime) {
+		setStatus(tasks, PlayerTaskStatus.IN_PROGRESS, now)
+	}
+
 
 	@Transactional
 	override fun generateTasks(playerId: Long) {
 		generateTasksProducer.send(playerId)
+	}
+
+	private fun setStatus(
+		playerTasks: Collection<PlayerTask>,
+		status: PlayerTaskStatus,
+		now: LocalDateTime
+	) {
+		playerTaskRepository.saveEntities(
+			playerTasks.map {
+				PlayerTask(it) {
+					this.status = status
+					updatedAt = now
+				}
+			},
+			SaveMode.UPDATE_ONLY
+		)
 	}
 }
