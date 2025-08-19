2e5d285cf540846eabec5381c3ca8043aa1e325c
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-08-19 17:15:58 +0300

    Adds player task topic initialization on user registration.

--- solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/service/user/UserRegistrationService.kt
+++ solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/service/service/user/UserRegistrationService.kt
@@ -2,15 +2,18 @@ package com.sleepkqq.sololeveling.player.service.service.user
 
 import com.sleepkqq.sololeveling.player.model.entity.player.Player
 import com.sleepkqq.sololeveling.player.model.entity.player.enums.LevelType
+import com.sleepkqq.sololeveling.player.model.entity.task.enums.TaskTopic
 import com.sleepkqq.sololeveling.player.model.entity.user.User
 import com.sleepkqq.sololeveling.player.service.service.player.LevelService
 import com.sleepkqq.sololeveling.player.service.service.player.PlayerBalanceService
+import com.sleepkqq.sololeveling.player.service.service.player.PlayerTaskTopicService
 import org.springframework.stereotype.Service
 
 @Service
 class UserRegistrationService(
 	private val levelService: LevelService,
-	private val playerBalanceService: PlayerBalanceService
+	private val playerBalanceService: PlayerBalanceService,
+	private val playerTaskTopicService: PlayerTaskTopicService
 ) {
 
 	private companion object {
@@ -23,6 +26,7 @@ class UserRegistrationService(
 			maxTasks = INITIAL_PLAYER_MAX_TASKS
 			level = levelService.initializeLevel(LevelType.PLAYER)
 			balance = playerBalanceService.initializePlayerBalance()
+			taskTopics = TaskTopic.entries.map { playerTaskTopicService.initialize(user.id, it) }
 		}
 	}
 }
