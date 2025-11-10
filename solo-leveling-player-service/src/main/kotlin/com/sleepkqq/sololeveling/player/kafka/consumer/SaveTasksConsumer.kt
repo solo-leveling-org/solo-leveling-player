81f0b2fd01af4cd5e426978997fdcc4818799b49
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-11-11 02:53:11 +0300

    Fix UUID assignment for task localization fields in SaveTasksConsumer

--- solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/kafka/consumer/SaveTasksConsumer.kt
+++ solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/kafka/consumer/SaveTasksConsumer.kt
@@ -4,6 +4,7 @@ import com.sleepkqq.sololeveling.avro.constants.KafkaGroupIds
 import com.sleepkqq.sololeveling.avro.constants.KafkaTaskTopics
 import com.sleepkqq.sololeveling.avro.task.SaveTasksEvent
 import com.sleepkqq.sololeveling.player.mapper.AvroMapper
+import com.sleepkqq.sololeveling.player.model.entity.task.dto.TaskInput
 import com.sleepkqq.sololeveling.player.service.notification.NotificationCommand
 import com.sleepkqq.sololeveling.player.service.notification.NotificationService
 import com.sleepkqq.sololeveling.player.service.player.PlayerTaskService
@@ -16,6 +17,7 @@ import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.util.Assert
+import java.util.UUID
 
 @Suppress("unused")
 @Service
@@ -64,9 +66,12 @@ class SaveTasksConsumer(
 	private fun processSaveTasksEvent(event: SaveTasksEvent) {
 		validateSaveTasksEvent(event)
 
-		val tasks = event.tasks.map {
-			avroMapper.map(it).toEntity()
-		}
+		val tasks = event.tasks.map(avroMapper::map)
+			.onEach {
+				it.title!!.id = UUID.randomUUID()
+				it.description!!.id = UUID.randomUUID()
+			}
+			.map(TaskInput::toEntity)
 
 		log.info("Updating {} tasks for player {}", tasks.size, event.playerId)
 		taskService.updateAll(tasks)
