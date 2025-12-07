7567660b536e2329592b29f4531570de8fec2ee6
Nikita <107306032+sleepkqq@users.noreply.github.com>
2025-12-07 23:34:16 +0300

    Logging interceptors (#63)
    * Update solo-leveling-lib version and refactor user query logic.
    
    * Update solo-leveling-lib version to 1.4.8.
    
    * Remove unused logger and Kafka send logging.
    
    * Update solo-leveling-lib version to 1.4.9.
    
    * Add enum type predicates for filtering player tasks.

--- solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/kafka/producer/SendNotificationProducer.kt
+++ solo-leveling-player-service/src/main/kotlin/com/sleepkqq/sololeveling/player/kafka/producer/SendNotificationProducer.kt
@@ -2,7 +2,6 @@ package com.sleepkqq.sololeveling.player.kafka.producer
 
 import com.sleepkqq.sololeveling.avro.constants.KafkaTaskTopics
 import com.sleepkqq.sololeveling.player.service.notification.NotificationService
-import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
@@ -11,21 +10,8 @@ class SendNotificationProducer(
 	private val kafkaTemplate: KafkaTemplate<String, Any>
 ) {
 
-	private val log = LoggerFactory.getLogger(javaClass)
-
 	fun send(data: NotificationService.NotificationData) {
 		val event = data.event
 		kafkaTemplate.send(KafkaTaskTopics.SEND_NOTIFICATION_TOPIC, event.txId, event)
-			.whenComplete { _, e ->
-				if (e == null) {
-					log.info(
-						"<< {} notification event sent | txId={}",
-						data.message.replaceFirstChar { it.uppercase() },
-						event.txId
-					)
-				} else {
-					log.error("Failed to send {} notification event | txId={}", data.message, event.txId, e)
-				}
-			}
 	}
 }
